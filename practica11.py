093e9ab015bddacba1187f8aab2d1c0f7cc9fa18
DanielaBarrios <103336142+DanielaBarrios@users.noreply.github.com>
2022-06-18 15:40:51 -0500

    Update practica11.py

--- practica11.py
+++ practica11.py
@@ -4,9 +4,6 @@ from matplotlib import pyplot as plt
 import math as ma
 import cv2 #opencv
 
-
-
-
 ########################### RECONOCIMIENTO   ###########################
 
 query_img = cv2.imread('gato.png')
@@ -19,18 +16,15 @@ train_img_bw = cv2.cvtColor(train_img, cv2.COLOR_BGR2GRAY)
 # Initialize the ORB detector algorithm
 orb = cv2.ORB_create()
   
-# Now detect the keypoints and compute the descriptors for the query image
-# and train image
+# Now detect the keypoints and compute the descriptors for the query image and train image
 queryKeypoints, queryDescriptors = orb.detectAndCompute(query_img_bw,None)
 trainKeypoints, trainDescriptors = orb.detectAndCompute(train_img_bw,None)
  
-# Initialize the Matcher for matching the keypoints and then match the
-# keypoints
+# Initialize the Matcher for matching the keypoints and then match the keypoints
 matcher = cv2.BFMatcher()
 matches = matcher.match(queryDescriptors,trainDescriptors)
   
-# draw the matches to the final image containing both the images the drawMatches()
-# function takes both images and keypoints and outputs the matched query image with
+# draw the matches to the final image containing both the images the drawMatches() function takes both images and keypoints and outputs the matched query image with
 # its train image
 final_img = cv2.drawMatches(query_img, queryKeypoints,
 train_img, trainKeypoints, matches[:20],None)
@@ -41,4 +35,29 @@ final_img = cv2.resize(final_img, (1000,650))
 cv2.imshow("Matches", final_img)
 
 cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+############################ Deteccion   ###########################
+cap = cv2.VideoCapture('video.mp4')
+fgbg = cv2.createBackgroundSubtractorMOG2()
+
+while(1):
+    ret, frame = cap.read()
+    kernel = np.ones((5,5),np.uint8)
+    fgmask = fgbg.apply(frame)
+    opening = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+    res_ope = cv2.bitwise_and(frame,frame, mask= opening)
+    res_r = cv2.bitwise_and(frame,frame, mask= fgmask)
+    cv2.imshow('fgmask',frame)
+    cv2.imshow('frame',fgmask)
+    cv2.imshow('x',res_r)
+    cv2.imshow('res_open',res_ope)
+
+    
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+    
+
+cap.release()
 cv2.destroyAllWindows()
